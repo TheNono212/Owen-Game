3a253f9d97cee7b36f5ec420a2cf2fdaa7908b01
TheNono212 <Yato.nono.Valdez@gmail.com>
2023-10-30 16:00:14 +0100

    weapon holding

---
+++ Owen's Revolution/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HO
+{
+    public class PlayerInventory : MonoBehaviour
+    {
+        WeaponSlotManager weaponSlotManager;
+
+        public WeaponItem rightWeapon;
+        public WeaponItem leftWeapon;
+
+        private void Awake() 
+        {
+            weaponSlotManager = GetComponentInChildren<WeaponSlotManager>();
+        }
+        void Start()
+        {
+            weaponSlotManager.LoadWeaponOnSlot(rightWeapon, false);
+            weaponSlotManager.LoadWeaponOnSlot(leftWeapon, true);
+        }
+    }
+}
